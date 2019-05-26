8e61ddf48e9ed724603a9e461e8f56d4067b253e
Zach Lerew <lerewz@oregonstate.edu>
2019-05-26 16:56:36 -0700

    Summary total

--- WebApp/ClientApp/components/LoanRow.tsx
+++ WebApp/ClientApp/components/LoanRow.tsx
@@ -8,19 +8,31 @@ import { Helpers } from '../api/Helpers';
 interface Props {
     loan: ILoan;
     linkTo: string;
+    updateFunc: (n: number, m: number) => any;
 }
 
 interface State {
     helpers: Helpers;
+    totalDailyCost: number;
+    partialDailyCost: number;
 }
 
 // Shows information for a single loan, links to detailed view of loan
 export class LoanRow extends React.Component<Props, State> {
     constructor(props: Props, state: State) {
         super(props, state);
+
+        let scopedHelpers = new Helpers();
+
         this.state = {
-            helpers: new Helpers()
+            helpers: scopedHelpers,
+            partialDailyCost: scopedHelpers.calculateCurrentCost(this.props.loan.currentPayment),
+            totalDailyCost: scopedHelpers.calculateDailyCost(this.props.loan.currentPayment)
         };
+    }
+
+    public componentWillMount() {
+        this.props.updateFunc(this.state.partialDailyCost, this.state.totalDailyCost);
     }
 
     public render() {
@@ -32,13 +44,13 @@ export class LoanRow extends React.Component<Props, State> {
                 <div className='col-sm text-right font-weight-bold'>
                     <div>
                         <div className='d-inline-flex bd-highlight text-success'>
-                            {this.state.helpers.formatCurrency(this.state.helpers.calculateCurrentCost(this.props.loan.currentPayment))}
+                            {this.state.helpers.formatCurrency(this.state.partialDailyCost)}
                         </div>
                         <div className='d-inline-flex bd-highlight px-2'>
                             of
                         </div>
                         <div className='d-inline-flex bd-highlight'>
-                            {this.state.helpers.formatCurrency(this.state.helpers.calculateDailyCost(this.props.loan.currentPayment))} Paid Today
+                            {this.state.helpers.formatCurrency(this.state.totalDailyCost)} Paid Today
                         </div>
                     </div>
                 </div>
--- WebApp/ClientApp/components/LoanSummary.tsx
+++ WebApp/ClientApp/components/LoanSummary.tsx
@@ -3,13 +3,17 @@ import { RouteComponentProps, Redirect } from 'react-router';
 import { ILoan } from '../interfaces/ILoan';
 import { LoanRow } from './LoanRow';
 import { Api } from '../api/Api';
+import { Helpers } from '../api/Helpers';
 
 interface Props {
 }
 
 interface State {
+    helpers: Helpers;
     api: Api;
     loans: Array<ILoan>;
+    totalDailyCost: Array<number>;
+    partialDailyCost: Array<number>;
 }
 
 // Shows account details for a logged in user
@@ -17,9 +21,27 @@ export class LoanSummary extends React.Component<Props, State> {
     constructor(props: Props, state: State) {
         super(props, state);
         this.state = {
+            helpers: new Helpers(),
             api: new Api(),
-            loans: []
+            loans: [],
+            totalDailyCost: [],
+            partialDailyCost: []
         };
+
+        this.updateCostFunc = this.updateCostFunc.bind(this);
+    }
+
+    updateCostFunc(partial: number, total: number) {
+        var partialArray = this.state.partialDailyCost;
+        partialArray.push(partial);
+
+        var totalArray = this.state.totalDailyCost;
+        totalArray.push(total);
+
+        this.setState({
+            totalDailyCost: totalArray,
+            partialDailyCost: partialArray
+        });
     }
 
     // Lifecycle
@@ -35,15 +57,32 @@ export class LoanSummary extends React.Component<Props, State> {
     public render() {
         // Render each loan
         const loansJSX = this.state.loans.map(loan => (
-            <LoanRow loan={loan} linkTo={'/loans/' + loan.id}/>
+            <LoanRow loan={loan} linkTo={'/loans/' + loan.id} updateFunc={this.updateCostFunc}/>
         ));
 
         return (
             <div id='user-details'>
                 <div className='row h-100'>
                     <div className='col'>
-                        <p className='bd-lead'>Your Loans</p>
-                        {loansJSX}
+                        <div className='bd-lead'>
+                            <div className='d-inline-flex bd-highlight px-2'>
+                                You have paid 
+                            </div>
+                            <div className='d-inline-flex bd-highlight text-success'>
+                                {this.state.helpers.formatCurrency(this.state.partialDailyCost.reduce((a, b) => { return a + b }, 0))} 
+                            </div>
+                            <div className='d-inline-flex bd-highlight px-2'>
+                                out of
+                            </div>
+                            <div className='d-inline-flex bd-highlight'>
+                                {this.state.helpers.formatCurrency(this.state.totalDailyCost.reduce((a, b) => { return a + b }, 0))}
+                            </div>
+                            <div className='d-inline-flex bd-highlight px-2'>
+                                today
+                            </div>
+                        </div>
+
+                        {this.state.loans ? loansJSX : "No Data"}
                     </div>
                 </div>
             </div>
